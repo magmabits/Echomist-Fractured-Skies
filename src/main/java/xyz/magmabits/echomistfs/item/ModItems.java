4210c99e3d0ae389268588748acb9fc294e198a9
Magma <sacred.lunar.dragon@gmail.com>
2026-01-05 16:31:59 +0100

    - Fixed Halberd Repair Material - Fixed Halberd Attack Damage and Attack Speed

--- src/main/java/xyz/magmabits/echomistfs/item/ModItems.java
+++ src/main/java/xyz/magmabits/echomistfs/item/ModItems.java
@@ -6,12 +6,13 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import xyz.magmabits.echomistfs.Echomistfs;
+import xyz.magmabits.echomistfs.util.SylviteToolMaterial;
 
 public class ModItems {
     public static final Item SYLVITE_SHARD = registerItem("sylvite_shard", new Item(new Item.Settings()));
     public static final Item SYLVITE_HALBERD = registerItem("sylvite_halberd",
-            new SylviteHalberd(ToolMaterials.DIAMOND, new Item.Settings()
-                    .attributeModifiers(SylviteHalberd.createAttributeModifiers(ToolMaterials.DIAMOND, 4, -2.6f))));
+            new SylviteHalberd(SylviteToolMaterial.SYLVITE, new Item.Settings()
+                    .attributeModifiers(SylviteHalberd.createAttributeModifiers(SylviteToolMaterial.SYLVITE, 5, -2.8f))));
     public static final Item SYLVITE_TOKEN = registerItem("sylvite_token", new SylviteToken(new Item.Settings().maxCount(1)));
 
     private static Item registerItem(String name, Item item) {
